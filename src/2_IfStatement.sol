636a6170c0e4bd51607ff5f2a4bb72353b81b9f8
RareSkills <114334277+RareSkills@users.noreply.github.com>
2023-02-16 22:40:59 +0800

    Update 2_IfStatement.sol

--- src/2_IfStatement.sol
+++ src/2_IfStatement.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.13;
 
 contract IfStatement {
+    ///@notice return the a if a is larger, and b if b is larger. Return either if they are the same.
     function max(uint256 a, uint256 b) public pure returns (uint256) {
         // return the maximum of a and b
     }
 
+    ///@notice return the a if a is smaller, and b if b is smaller. Return either if they are the same.
     function min(uint256 a, uint256 b) public pure returns (uint256) {
         // return the minimum of a and b
     }
