ca00140468471168b48f8f51dafb9fc81e92b3b3
Jesserc <raymondjesse713@gmail.com>
2023-03-09 12:32:53 +0100

    fixes

--- PriceIsRight/src/PriceIsRight.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.13;
-
-contract PriceIsRight {
-    /// @dev make this function revert unless exactly 1 ether is sent as value in the function call
-    function buy() public payable {
-        /// your code here
-    }
-}
--- PublicVariable/src/PublicVariable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.13;
-
-contract PublicVariable {
-    // change this code so this number
-    // is visible outside the contract
-    uint256 private number = 42;
-}
