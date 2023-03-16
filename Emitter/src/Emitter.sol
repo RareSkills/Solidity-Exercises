61c2ccf76ef1ba8562dca9c5188334494ce18dfa
AmadiMichael <amadimichaeld@gmail.com>
2023-03-16 13:15:13 +0100

    improve emitter comments

--- Emitter/src/Emitter.sol
+++ Emitter/src/Emitter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 contract Emitter {
     /* This exercise assumes you know how events work.
-        1. Create a event that emits two values; `address` and `uint256`.
+        1. Create a event that emits two non-indexed values; `address` and `uint256`.
         2. Emit the event in the trigger function below
         3. The name of the event must be `Trigger`
     */
