ddb9541ee6cad402e498bde51f06308350bfca7d
Tanim0la <36541366+tanim0la@users.noreply.github.com>
2023-05-04 11:51:48 +0100

    Update AccessModifiers.sol

--- AccessModifiers/src/AccessModifiers.sol
+++ AccessModifiers/src/AccessModifiers.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
-// You may not modify this contract
+// You may modify this contract
 contract Parent {
     uint256 private _value;
 }
