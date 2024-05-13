39ecea0e7fe8f7e516ba19af46d5f26a24781172
Yamini-Github <yamini.chaudhari18@gmail.com>
2024-05-13 22:30:02 +0530

    Update PublicVariable.sol

--- PublicVariable/src/PublicVariable.sol
+++ PublicVariable/src/PublicVariable.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
-
+uint256 public number;
 contract PublicVariable {
     // change this code so this number
     // is visible outside the contract
-    uint256 private number = 42;
+   // uint256 private number = 42;
 }
