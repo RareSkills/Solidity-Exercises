a7dc36add721188291109c82b4045be1d66d3dc5
0X123 <54817860+0xRepox@users.noreply.github.com>
2024-05-27 11:47:32 +0100

    Update Add.sol

--- Add/src/Add.sol
+++ Add/src/Add.sol
@@ -5,4 +5,11 @@ contract Add {
         function add(uint256 a, uint256 b) public pure returns (uint256) {
         return a + b;
     }
+
+        function testAddTwo(unit256 a, 0) public pure returns (unit256) {
+        return a +0;
+}
+
+        function testAddThree(unit256 a, unit256 b, unit256 c) public pure returns (unit256) {
+        return a + b
 }
