f8e0dbf74117c331486601d94ad7c69469a79105
0X123 <54817860+0xRepox@users.noreply.github.com>
2024-05-27 11:48:21 +0100

    Update Add.sol

--- Add/src/Add.sol
+++ Add/src/Add.sol
@@ -2,14 +2,4 @@
 pragma solidity ^0.8.13;
 
 contract Add {
-        function add(uint256 a, uint256 b) public pure returns (uint256) {
-        return a + b;
-    }
-
-        function testAddTwo(unit256 a, 0) public pure returns (unit256) {
-        return a +0;
-}
-
-        function testAddThree(unit256 a, unit256 b, unit256 c) public pure returns (unit256) {
-        return a + b
-}
+        
