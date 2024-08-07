ed39e56b4564da88f0c72aea519cd8072e0248e3
Mattia Alfonso <alfonsomattia@gmail.com>
2024-08-07 10:24:56 +0200

    Fixed test for Typecast
    (cherry picked from commit 91c189bdec5380dce9ad780726f3c4642a2131b2)

--- Typecast/test/Typecast.t.sol
+++ Typecast/test/Typecast.t.sol
@@ -16,12 +16,10 @@ contract TypecastTest is Test {
         typecast.typeCast{value: 1 ether}();
 
         bool success;
+        vm.deal(address(this), 0x3fe8e3f000000000000000000000000000000000000000000000000000000000);
 
         assembly {
-            mstore(
-                0x00,
-                0x3fe8e3f000000000000000000000000000000000000000000000000000000000
-            )
+            mstore(0x00, hex"12e9b56d")
             let addr := sload(typecast.slot)
             success := call(gas(), addr, addr, 0x00, 0x04, 0x00, 0x00)
         }
