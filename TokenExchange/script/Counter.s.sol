18b5889be9ecca6ab8b12bb42d6d2af4cfaa4b40
tanim0la <izukfred@gmail.com>
2024-09-27 10:57:07 +0100

    add TokenExchange exercise

--- TokenExchange/script/Counter.s.sol
+++ TokenExchange/script/Counter.s.sol
@@ -2,17 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
-
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-
-        counter = new Counter();
 
         vm.stopBroadcast();
     }
