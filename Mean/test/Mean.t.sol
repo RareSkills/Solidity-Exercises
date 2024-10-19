2332096f0ce772990e098d55f2366075571d95b8
Aashish Paliwal <aashish.paliwal@live.com>
2024-10-19 15:49:02 +0530

    structure and add tests for Mean contract, including edge cases and Panic error handling

--- Mean/test/Mean.t.sol
+++ Mean/test/Mean.t.sol
@@ -11,7 +11,7 @@ contract MeanTest is Test {
         mean = new Mean();
     }
 
-    function testMean() public {
+    function testMeanx() external {
         uint256[] memory x = new uint256[](5);
         x[0] = 5;
         x[1] = 10;
@@ -19,6 +19,10 @@ contract MeanTest is Test {
         x[3] = 20;
         x[4] = 25;
 
+        assertEq(mean.mean(x), 15, "expected mean of arr 'x' is 15");
+    }
+
+    function testMeany() external {
         uint256[] memory y = new uint256[](5);
         y[0] = 5;
         y[1] = 10;
@@ -26,6 +30,10 @@ contract MeanTest is Test {
         y[3] = 19;
         y[4] = 25;
 
+        assertEq(mean.mean(y), 14, "expected mean of array 'y' is 14");
+    }
+
+    function testMeanMostlyZeroes() external {
         uint256[] memory z = new uint256[](5);
         z[0] = 0;
         z[1] = 0;
@@ -33,8 +41,35 @@ contract MeanTest is Test {
         z[3] = 0;
         z[4] = 0;
 
-        assertEq(mean.mean(x), 15, "mean of arr 'x' is 15");
-        assertEq(mean.mean(y), 14, "mean of arr 'y' is 14");
-        assertEq(mean.mean(z), 0, "mean of arr 'z' is 0");
+        assertEq(mean.mean(z), 0, "expected mean of arr 'z' is 0");
+    }
+
+    function testMeanSingleElement() external {
+        uint256[] memory singleElement = new uint256[](1);
+        singleElement[0] = 42;
+        assertEq(
+            mean.mean(singleElement),
+            42,
+            "expected 42, because the answer to ultimate question of life is 42. Also, mean of a single-element array should be element itself"
+        );
+    }
+
+    function testMeanLargeNumbers() external {
+        uint256[] memory largeNumbers = new uint256[](3);
+        largeNumbers[0] = 1e42;
+        largeNumbers[1] = 1e42;
+        largeNumbers[2] = 1e42;
+
+        assertEq(
+            mean.mean(largeNumbers),
+            1e42,
+            "Mean of large numbers should be the large number itself"
+        );
+    }
+
+    function testMeanEmptyArray() external {
+        uint256[] memory emptyArray = new uint256[](0);
+        vm.expectRevert(abi.encodeWithSignature("Panic(uint256)", 0x12));
+        mean.mean(emptyArray);
     }
 }
